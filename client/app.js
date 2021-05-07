1e33480c45dd2c019cc52a57131c7d9494957af6
Rio <riostockton@gmail.com>
2021-05-07 10:46:38 +0100

    Completed form validation

--- client/app.js
+++ client/app.js
@@ -8,29 +8,63 @@ class Form {
 			password: "",
 			terms: false,
 		};
-		this.errorValues = { ...this.values };
+		this.errors = { ...this.values };
 		this.events();
 	}
 
 	events() {
 		this.form.addEventListener("change", e => this.handleFormChange(e));
-		this.submit.addEventListener("click", e => this.validateForm(e));
+		this.submit.addEventListener("click", e =>
+			this.handleFormValidation(e)
+		);
 	}
 
 	handleFormChange(e) {
-		const { name, value, checked, type } = e.target;
-		const state = type === "checkbox" ? checked : value;
+		const { name, value, checked } = e.target;
+		const string = typeof this.values[name] === "string";
+		const state = string ? value : checked;
 		this.values = {
 			...this.values,
 			[name]: state,
 		};
 	}
 
-	validateForm() {
-		e.preventDefault();
+	isFormValid() {
 		const { username, email, password, terms } = this.values;
-		let temp = [];
-		username.value === "" || username.length;
+		console.log(username.length);
+
+		this.errors.username =
+			username.length > 0 && username.length < 23
+				? ""
+				: "Username must not be empty and be no more than 22 characters";
+		this.errors.email = /$^|.+@.+..+/.test(email)
+			? ""
+			: "Email is invalid";
+		this.errors.password =
+			password.length > 5
+				? ""
+				: "Password must not be empty and be at least 6 characters";
+		this.errors.terms = terms
+			? ""
+			: "You must agree to the terms and conditions";
+		return Object.values(this.errors).every(e => e === "");
+	}
+
+	handleFormValidation(e) {
+		e.preventDefault();
+		const errorsContainer = document.getElementById("errors-container");
+		const errorList = document.getElementById("error-list");
+		errorList.innerHTML = "";
+
+		if (!this.isFormValid()) {
+			Object.keys(this.errors).map(error => {
+				const errorText = `<li>${this.errors[error]}</li>`;
+				errorList.insertAdjacentHTML("beforeend", errorText);
+			});
+			errorsContainer.style.display = "block";
+		} else {
+			window.location.href = "/";
+		}
 	}
 }
 
